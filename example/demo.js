c93b2ddaf752ce37d17bf4c1f626334933fcf579
lanyue <lanyueos@qq.com>
2021-12-15 23:13:38 +0800

    完成主要函数

--- example/demo.js
+++ example/demo.js
@@ -1,6 +1,43 @@
-const sqler = require('../dest')
+const sqlstring = require('sqlstring')
+const Sqler = require('../dest')
 
-const b = new sqler.Condition("where")
+const q = new Sqler.default()
+
+q.select('select id, username')
+q.from('users')
+q.where(where => {
+  where.and('field1 = ?', 1)
+  where.and('field2 = ?', 2)
+  where.or(or => {
+    or.add("field3 = ?", 3)
+    or.add("field4 = ?", 4)
+    or.and(and => {
+      and.and('field5 = ?', 5)
+      and.and('field6 = ?', 6)
+      and.or(or => {
+        or.add("field7 = ?", 7)
+        or.add("field8 = ?", 8)
+      })
+    })
+  })
+})
+q.group(group => {
+  group.add('field1')
+  group.add('field2')
+  group.add('field3')
+})
+q.having(having => {
+  having.and('field1 > 1')
+})
+q.order(o => {
+  o.add('id', 'desc')
+  o.add('age', 'asc')
+})
+q.limit(0, 10)
+console.log(q.do())
+console.log(q.doCount())
+
+const b = new Sqler.Condition("where")
 
 b.and("field1 = ?", 1)
 b.and("field2 = ?", 2)
@@ -16,4 +53,15 @@ b.or(or => {
     })
   })
 })
-console.log(b.do())
+console.log(b.do(true))
+console.log(sqlstring.format('like ?', []))
+
+const o = new Sqler.Order()
+o.add('field1', 'asc')
+o.add('field2', 'desc')
+console.log(o.do())
+
+const g = new Sqler.Group()
+g.add('field1')
+g.add('field2', 'field3')
+console.log(g.do())
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { format } from 'sqlstring'
+
 // 减少括号层次
 function brackets (sql: string) :string {
   return sql.replace(/^\((.*)\)$/g, '$1')
@@ -50,7 +52,7 @@ class Or {
   and (andFn: (and: Condition) => void) {
     const and = new Condition()
     andFn(and)
-    const {sql, args} = and.do()
+    const {sql, args} = and.do(false)
     if (sql) {
       this.add(`(${brackets(sql)})`, ...args)
     }
@@ -85,9 +87,57 @@ export class Condition {
     }
   }
 
-  do (): {sql: string, args: any[]} {
-    const { sql, args } = this.block.do(" and ")
-    if (!this.name || !sql) return { sql, args }
-    return {sql: this.name + ' ' + sql, args}
+  do (isFormat: boolean = true): {sql: string, args: any[]} {
+    let { sql, args } = this.block.do(" and ")
+  
+    if (sql && this.name) {
+      sql =  this.name + ' ' + sql
+    }
+
+    if (isFormat) {
+      return { sql: format(sql, args), args: [] }
+    }
+
+    return { sql, args }
+  }
+}
+
+export class Order {
+  private block: Block
+  constructor () {
+    this.block = new Block()
+  }
+
+  add (field: string, sort: 'desc' | 'asc') {
+    this.block.add(`${field} ${sort}`)
+  }
+
+  do () {
+    const { sql } = this.block.do(', ')
+    if (!sql) {
+      return ''
+    }
+    return 'order by ' + sql
+  }
+}
+
+export class Group {
+  private block: Block
+  constructor () {
+    this.block = new Block()
+  }
+
+  add (...field: string[]) {
+    for (const i in field) {
+      this.block.add(field[i])
+    }
+  }
+
+  do () {
+    const { sql } = this.block.do(', ')
+    if (!sql) {
+      return ''
+    }
+    return 'group by ' + sql
   }
 }
